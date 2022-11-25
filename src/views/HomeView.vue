bf20f8bcc95ec88d0580f6266d729aacc4cb2a64
Jandronit <jandrohen@gmail.com>
2022-11-25 21:05:41 +0100

    (lesson 158 course vue) Structure modular

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,5 +3,15 @@
     <img alt="Vue logo" src="../assets/logo.png">
   </div>
   <h1>Hola VUE</h1>
-  <button class="btn btn-primary">Primary</button>
+  <button @click="goToDaybook" class="btn btn-primary">Primary</button>
 </template>
+
+<script>
+export default {
+ methods: {
+   goToDaybook() {
+     this.$router.push({ name: 'daybook' })
+   }
+ }
+}
+</script>
