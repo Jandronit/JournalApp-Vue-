eae3825fc2a191b1c59025de077c85d39d1f6c75
Jandronit <jandrohen@gmail.com>
2022-12-04 12:29:39 +0100

    (lesson 160 course vue) Component EntryList and Entry

---
+++ src/modules/daybook/components/Entry.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="entry-container mb-3 pointer p-2">
+<!--  Tittle-->
+  <div class="entry-title d-flex">
+    <span class="text-success fs-5 fw-bold">4</span>
+    <span class="mx-1 fs-5">Diciembre</span>
+    <span class="mx-2 fw-light">2022, Domingo</span>
+  </div>
+<!--  Content-->
+  <div class="entry-description">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, adipisci, alias, amet animi asperiores atque autem beatae consectetur consequatur corporis cumque debitis dignissimos doloremque dolorum ea eius enim eos esse est et eum ex expedita explicabo facilis fuga fugiat fugit harum hic id illum impedit in incidunt inventore ipsa ipsam ipsum iste iure iusto laboriosam laborum laudantium libero magnam magni maiores maxime minima
+  </div>
+</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+
+.entry-container{
+  border-bottom: 1px solid #2c3e50;
+  transition: 0.3s all ease-in;
+
+  &:hover{
+    background-color: lighten($color: #2c3e50, $amount: 45%);
+    transition: 0.3s all ease-in;
+  }
+}
+
+.entry-description{
+  font-size: 0.75rem;
+}
+</style>
--- src/modules/daybook/components/EntryList.vue
+++ src/modules/daybook/components/EntryList.vue
@@ -1,7 +1,39 @@
 <template>
-<h1>EntryList!!!</h1>
+<div class="entry-list-container">
+  <div class="px-2 pt-2">
+    <input type="text"
+           class="form-control"
+           placeholder="Buscar entrada"/>
+
+    <div class="entry-scrollbar">
+      <Entry v-for="item in 100" :key="item">
+
+      </Entry>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
-export default {}
+
+import {defineAsyncComponent} from 'vue';
+
+export default {
+  components: {
+  Entry: defineAsyncComponent(() => import('./Entry.vue'))
+    },
+}
 </script>
+
+<style lang="scss" scoped>
+
+.entry-list-container{
+  border-right: 1px solid #2c3e50;
+  height: calc(100vh - 56px);
+}
+
+.entry-scrollbar{
+  height: calc(100vh - 110px);
+  overflow: scroll;
+}
+</style>
