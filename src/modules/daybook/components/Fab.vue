965789c1fd1c209d00ed19d03f5639a4b05e196c
Jandronit <jandrohen@gmail.com>
2022-12-11 21:26:11 +0100

    (lesson 161 course vue) Add component NoEntrySelected

---
+++ src/modules/daybook/components/Fab.vue
@@ -0,0 +1,21 @@
+<template>
+<button  class="btn btn-primary">
+  <i class="fas fa-plus"></i>
+</button>
+</template>
+
+<script>
+export default {
+  name: "Fab"
+}
+</script>
+
+<style lang="scss" scoped>
+button{
+  border-radius: 50%;
+  bottom: 2rem;
+  position: fixed;
+  right: 2rem;
+  z-index: 99;
+}
+</style>
