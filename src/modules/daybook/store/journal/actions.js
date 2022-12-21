b595ed104f989534bc7cb0ee2e274c3c353f8328
Jandronit <jandrohen@gmail.com>
2022-12-21 20:26:54 +0100

    (lesson 165 course vue) Vuex module

---
+++ src/modules/daybook/store/journal/actions.js
@@ -0,0 +1,16 @@
+
+// export const myAction = async ({ commit }) => {
+//   // do something
+// }
+
+export const loadEntries = async (/*{ commit }*/) => {
+
+}
+
+export const updateEntry = async (/*{ commit }*/) => {
+
+}
+
+export const createEntry = async (/*{ commit }*/) => {
+
+}
